9718933a6bdaa5af6fc942eb17539499d9e887da
woooo96 <117805482+woooo96@users.noreply.github.com>
2022-11-18 15:00:47 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,25 +3,9 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        int num=1;
-        int count=1;
-        
-        while(true) {
-            if (n/num < 10) {
-                break;
-            }
-            num*=10;
-            count++;
-        }
-        
-        for (int i=0; i<count; i++){
-            int temp=n/num;
-            n-=temp*num;
-            answer+=temp;
-            num/=10;
-        }
 
-
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
 
         return answer;
     }
