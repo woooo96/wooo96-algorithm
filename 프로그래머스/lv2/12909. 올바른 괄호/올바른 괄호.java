e8a0dcd322edc8fb857e25557c6bd7d43c8f4978
Jinwoo Choi <117805482+woooo96@users.noreply.github.com>
2023-01-27 16:54:16 +0900

    [level 2] Title: 올바른 괄호, Time: 6.64 ms, Memory: 54.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,25 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int count =0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i)=='(') {
+                count++;
+            } else {
+                count--;
+            }
+            if (count<0) {
+                answer=false;
+                return answer;
+            }
+        }
+
+        if (count==0) {
+            answer = true;
+        } else {
+            answer = false;
+        }
+
+        return answer;
+    }
+}
