9c298e3fe7b940cadd50eeeb18a26152bf9e8b65
Jinwoo Choi <117805482+woooo96@users.noreply.github.com>
2023-02-28 18:21:02 +0900

    [level 2] Title: 구명보트, Time: 21.34 ms, Memory: 56.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -2,27 +2,21 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        List<Integer> peopleList = new ArrayList<>();
-        Arrays.sort(people);
-        for (int i = 0; i < people.length; i++) {
-            peopleList.add(i, people[i]);
-        }
 
-        int i=1;
-        while(peopleList.size()>0) {
-            if (peopleList.size()-i==0) {
-                peopleList.remove(0);
-                i=1;
-                answer++;
-                continue;
+        Arrays.sort(people);
+
+        int max = people.length - 1;
+        int min = 0;
+
+        while (true) {
+            int weight = people[max];
+            if (weight + people[min] <= limit) {
+                min++;
             }
-            if (peopleList.get(0)+peopleList.get(peopleList.size()-i) <= limit) {
-                peopleList.remove(0);
-                peopleList.remove(peopleList.size()-i);
-                i=1;
-                answer++;
-            } else {
-                i++;
+            max--;
+            answer++;
+            if(max < min) {
+                break;
             }
         }
         return answer;
