d108db80272f7f7571709069b272eadb00bab36d
Jinwoo Choi <117805482+woooo96@users.noreply.github.com>
2023-03-02 19:00:44 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -1,42 +1,6 @@
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        int[] array = new int[n];
-        for (int i = 0; i < array.length; i++) {
-            array[i]=1;
-        }
-        for (int i = 0; i < lost.length; i++) {
-            array[lost[i]-1]--;
-        }
-        for (int i = 0; i < reserve.length; i++) {
-            array[reserve[i]-1]++;
-        }
-
-        if(array[0]==0 && array[1]==2) {
-            array[0]=1;
-            array[1]=1;
-        }
-
-        for (int i = 1; i < array.length-1; i++) {
-            if(array[i]==0 && array[i-1]==2) {
-                array[i]=1;
-                array[i-1]=1;
-            }
-            if(array[i]==0 && array[i+1]==2) {
-                array[i]=1;
-                array[i+1]=1;
-            }
-        }
-
-        if(array[array.length-1]==0 && array[array.length-2]==2) {
-            array[array.length-1]=1;
-            array[array.length-2]=1;
-        }
-
-        for (int i = 0; i < array.length; i++) {
-            if(array[i]>=1) answer++;
-        }
-        
         return answer;
     }
 }
