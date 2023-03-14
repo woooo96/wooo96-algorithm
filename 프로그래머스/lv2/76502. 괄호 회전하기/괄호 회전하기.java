dc6e1ccaa942012420d56d46199050f8c87766bc
Jinwoo Choi <117805482+woooo96@users.noreply.github.com>
2023-03-14 17:50:24 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.29 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,8 +3,11 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
+        //s의 길이만큼 반복문을 돌면서 회전
         for (int i = 0; i < s.length(); i++) {
+            //Stack을 생성하여 회전한 값들에 대해서 stack에 저장
             Stack<Character> sStack = new Stack<>();
+            //Stack에 저장되는 처음 값이 ')', ']', '}' 의 경우 check 변수에 false저장
             Boolean check = true;
             for (int j = 0; j < s.length(); j++) {
                 int index = j+i>=s.length() ? j+i-s.length() : j+i;
@@ -12,6 +15,7 @@ class Solution {
                     check = false;
                     break;
                 }
+                //stack에 저장되어 있는 마지막 문자와 stack에 저장하려 하는 문자를 비교하여 서로 연관된 괄호라면 stack에서 지움
                 if((sStack.size()>=1 && sStack.peek()=='(') && s.charAt(index)==')') {
                     sStack.pop();
                     continue;
@@ -25,6 +29,7 @@ class Solution {
                 sStack.push(s.charAt(index));
             }
 
+            //stack에 저장되어있는게 없고 check값이 true면 answer 변수에 ++
             if (sStack.size()==0 && check==true) answer++;
         }
         
