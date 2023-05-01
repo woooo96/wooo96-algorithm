01819c78681606b6906beb85498ce1ecbb099173
Jinwoo Choi <117805482+woooo96@users.noreply.github.com>
2023-05-01 16:24:13 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 204 ms, Memory: 17548 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.next();
+        System.out.println(str.length());
+    }
+}
