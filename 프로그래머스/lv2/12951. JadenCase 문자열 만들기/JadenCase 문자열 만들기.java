409c51bb223d00dbd19acc33f06f54f029a88c1b
Jinwoo Choi <117805482+woooo96@users.noreply.github.com>
2023-05-03 13:46:58 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 2.80 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,19 +1,18 @@
 import java.util.*;
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        s = s.toLowerCase();
-        Stack<Character> c = new Stack<>();
-        if(s.charAt(0)>=97 && s.charAt(0)<=122) c.push((char) (s.charAt(0)-32));
-        else c.push(s.charAt(0));
-        answer+=c.peek();
-
-        for (int i = 1; i < s.length(); i++) {
-            if(c.peek()==' ' && s.charAt(i)>=97 && s.charAt(i)<=122) c.push((char) (s.charAt(i)-32));
-            else c.push(s.charAt(i));
-            answer+=c.peek();
+        String answer="";
+        Stack<String> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if(st.isEmpty() || st.peek().equals(" ")) {
+                st.push(String.valueOf(s.charAt(i)).toUpperCase());
+                answer+=st.peek();
+            }
+            else {
+                st.push(String.valueOf(s.charAt(i)).toLowerCase());
+                answer+=st.peek();
+            }
         }
-
         return answer;
-    }
+}
 }
