11acb96058d8c64abfbbf6c56737b67ac62df91e
Jinwoo Choi <117805482+woooo96@users.noreply.github.com>
2023-05-03 11:43:43 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -1,12 +1,16 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for (int i=1; i<=n/2; i++) {
+        for (int i=1; i<=Math.sqrt(n); i++) {
             if (n%i==0) {
                 answer+=i;
+                if(i!=n/i) {
+                    answer+=n/i;
+                }
+                
             }
         }
 
-        return answer+n;
+        return answer;
     }
 }
