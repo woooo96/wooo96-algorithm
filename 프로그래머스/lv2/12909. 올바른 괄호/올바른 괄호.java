03351b76b870bad30e106212d5582d6ed9c710b4
Jinwoo Choi <117805482+woooo96@users.noreply.github.com>
2023-08-13 01:22:35 +0900

    [level 2] Title: 올바른 괄호, Time: 20.51 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,14 +1,20 @@
 import java.util.*;
 class Solution {
-public boolean solution(String s) {
-        boolean answer=false;
-        Stack<Character> st = new Stack<>();
-        if(s.charAt(0)==')') return answer;
-        for (int i = 0; i < s.length(); i++) {
-            if(st.isEmpty() || s.charAt(i)=='(') st.push(s.charAt(i));
-            else st.pop();
-        }
-        answer = st.isEmpty() ? true : false;
-        return answer;
-    }
+	public boolean solution(String s) {
+		boolean answer=false;
+        //주어진 문자열에서 첫번째 문자가 닫는 괄호일 경우 false 리턴
+		if(s.charAt(0)==')') return answer;
+		
+		Stack<Character> st = new Stack<>();
+		for(int i=0; i<s.length(); i++) {
+            //문자열에서 여는 괄호인 경우 스택에 추가
+			if(s.charAt(i)=='(') st.add(s.charAt(i));
+            //스택이 비어있지 않고(==스택에 여는괄호가 있고) 문자열에 닫는 괄호가 올 경우 가장 마지막에 추가한 여는 괄호 스택에서 삭제
+			else if(!st.isEmpty() && s.charAt(i)==')') st.pop(); 
+            //올바르지 않은 케이스의 경우 false 값 리턴
+			else return answer;
+		}
+		if(st.isEmpty()) answer=true;
+		return answer;
+	}
 }
