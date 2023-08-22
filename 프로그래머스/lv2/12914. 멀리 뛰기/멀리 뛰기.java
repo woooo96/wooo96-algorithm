300d5fae7278d354fb005add1760cb4bf9fd916b
Jinwoo Choi <117805482+woooo96@users.noreply.github.com>
2023-08-22 21:32:11 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.17 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,14 +1,17 @@
 class Solution {
     public long solution(int n) {
         long answer = 0;
-        int[] arr = new int[2001];
-        arr[1] = 1;
-        arr[2] = 2;
+        //n의 범위대로 배열 생성
+        int[] arr = new int[2000];
+        //n=1일때와 n=2일때의 값을 미리 지정
+        arr[0] = 1;
+        arr[1] = 2;
 
-        for (int i = 3; i < n+1; i++) {
+        //점화식을 이용한 값 구하기
+        for (int i = 2; i < n; i++) {
             arr[i] = (arr[i-2] + arr[i-1]) % 1234567;
         }
-        answer=arr[n];
+        answer=arr[n-1];
         return answer;
     }
 }
