ef4331a068659ef68c2be3c70186f262c275a577
Jinwoo Choi <117805482+woooo96@users.noreply.github.com>
2024-04-28 22:40:53 +0900

    [level 2] Title: 모음 사전, Time: 0.03 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(String word) {
         int answer = 0;
-        int[] weights = {781, 156, 31, 6, 1}; // 각 자리수마다 가질 수 있는 가중치
+        int[] weights = {781, 156, 31, 6, 1};
         char[] chars = {'A', 'E', 'I', 'O', 'U'};
         
         for(int i=0; i<word.length(); i++) {
