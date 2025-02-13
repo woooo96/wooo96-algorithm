364040377737656067d38a345d2e705f15656416
Jinwoo Choi <117805482+woooo96@users.noreply.github.com>
2025-02-13 22:32:46 +0900

    [Bronze I] Title: 평균, Time: 212 ms, Memory: 18512 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner s=new Scanner(System.in);
-        int sub=s.nextInt();
-        int a[]=new int[sub];
-        double sum=0;
-        for (int i=0; i<a.length; i++){
-            a[i]=s.nextInt();
-        }
-        double max=a[0];
-        for (int n=1; n<a.length; n++){
-            if(max<a[n]) max=a[n];
-        }
-        for (int m=0; m<a.length; m++){
-            sum=sum+a[m]/max*100;
-        }
-        System.out.println(sum/sub);
-    }
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		//입력받을 과목의 수 
+		int N = sc.nextInt();
+		
+		int max = 0;
+		int sum = 0;
+		for(int i = 0; i < N; i ++) {
+			int tmp = sc.nextInt();
+			sum += tmp;
+			if (tmp > max) max = tmp;
+		}
+		System.out.print(sum*100.0 / max / N);	
+	}
 }
